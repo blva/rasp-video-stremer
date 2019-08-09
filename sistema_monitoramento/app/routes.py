9c2fc484e894c3634f76053d5e7a011507759f76
Bianca <blvaguiar@gmail.com>
2019-08-09 02:55:02 +0000

    add camera

--- sistema_monitoramento/app/routes.py
+++ sistema_monitoramento/app/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, Response
 from app import app
 from app.forms import LoginForm
+from camera import VideoCamera
 
 @app.route('/')
 def home():
@@ -17,5 +18,17 @@ def connect():
         return redirect('/reboot')
     return render_template('login.html', title='Sign In', form=form)
 
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
