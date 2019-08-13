075216a296737c0a9c0450477501161c7fa3e9e9
Bianca <blvaguiar@gmail.com>
2019-08-13 14:37:21 +0000

    Add image process

--- sistema_monitoramento/app/camera.py
+++ sistema_monitoramento/app/camera.py
@@ -2,73 +2,73 @@ import cv2
 import picamera
 
 class VideoCamera(object):
-    def __init__(self):
-        # Using OpenCV to capture from device 0. If you have trouble capturing
-        # from a webcam, comment the line below out and use a video file
-        # instead.
-        self.video = cv2.VideoCapture(0)
-        self.firstFrame = None
-        # If you decide to use video.mp4, you must have this file in the folder
-        # as the main.py.
-        # self.video = cv2.VideoCapture('video.mp4')
-    
-    def __del__(self):
-        self.video.release()
-    
-    def process_imgage(self,image):
-        # frame = frame if args.get("video", None) is None else frame[1]
-        text = "Unoccupied"
+	def __init__(self):
+		# Using OpenCV to capture from device 0. If you have trouble capturing
+		# from a webcam, comment the line below out and use a video file
+		# instead.
+		self.video = cv2.VideoCapture(0)
+		self.firstFrame = None
+		# If you decide to use video.mp4, you must have this file in the folder
+		# as the main.py.
+		# self.video = cv2.VideoCapture('video.mp4')
+	
+	def __del__(self):
+		self.video.release()
+	
+	def process_imgage(self,image):
+		# frame = frame if args.get("video", None) is None else frame[1]
+		text = "Unoccupied"
 
-        # if the frame could not be grabbed, then we have reached the end
-        # of the video
-        # if frame is None:
-        # 	break
+		# if the frame could not be grabbed, then we have reached the end
+		# of the video
+		# if frame is None:
+		# 	break
 
-        # resize the frame, convert it to grayscale, and blur it
-        frame = cv2.resize(frame, (500,500), cv2.INTER_AREA)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        gray = cv2.GaussianBlur(gray, (21, 21), 0)
+		# resize the frame, convert it to grayscale, and blur it
+		frame = cv2.resize(frame, (500,500), cv2.INTER_AREA)
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-        # if the first frame is None, initialize it
-        if firstFrame is None:
-            firstFrame = gray
+		# if the first frame is None, initialize it
+		if self.firstFrame is None:
+			self.firstFrame = gray
 
-        # compute the absolute difference between the current frame and
-        # first frame
-        frameDelta = cv2.absdiff(firstFrame, gray)
-        thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+		# compute the absolute difference between the current frame and
+		# first frame
+		frameDelta = cv2.absdiff(self.firstFrame, gray)
+		thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
 
-        # dilate the thresholded image to fill in holes, then find contours
-        # on thresholded image
-        thresh = cv2.dilate(thresh, None, iterations=2)
-        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-            cv2.CHAIN_APPROX_SIMPLE)
-        cnts = imutils.grab_contours(cnts)
+		# dilate the thresholded image to fill in holes, then find contours
+		# on thresholded image
+		thresh = cv2.dilate(thresh, None, iterations=2)
+		cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+			cv2.CHAIN_APPROX_SIMPLE)
+		cnts = imutils.grab_contours(cnts)
 
-        # loop over the contours
-        for c in cnts:
-            # if the contour is too small, ignore it
-            if cv2.contourArea(c) < args["min_area"]:
-                continue
+		# loop over the contours
+		for c in cnts:
+			# if the contour is too small, ignore it
+			if cv2.contourArea(c) < args["min_area"]:
+				continue
 
-            # compute the bounding box for the contour, draw it on the frame,
-            # and update the text
-            (x, y, w, h) = cv2.boundingRect(c)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            text = "Occupied"
+			# compute the bounding box for the contour, draw it on the frame,
+			# and update the text
+			(x, y, w, h) = cv2.boundingRect(c)
+			cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+			text = "Occupied"
 
-        # draw the text and timestamp on the frame
-        cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-        cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
-            (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+		# draw the text and timestamp on the frame
+		cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+		cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
+			(10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
 
-    def get_frame(self):
-        success, image = self.video.read()
-        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
-        # so we must encode it into JPEG in order to correctly display the
-        # video stream.
-        
-        ret, jpeg = cv2.imencode('.jpg', image)
-        return jpeg.tobytes()
-    
+	def get_frame(self):
+		success, image = self.video.read()
+		# We are using Motion JPEG, but OpenCV defaults to capture raw images,
+		# so we must encode it into JPEG in order to correctly display the
+		# video stream.
+		
+		ret, jpeg = cv2.imencode('.jpg', image)
+		return jpeg.tobytes()
+	
